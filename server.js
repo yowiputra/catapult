5ec70ffabaf30642d1ae07d1b18540e40e6b0b56
Yohan Wiputra <yowiputra@gmail.com>
2017-08-29 16:03:14 +0000

    added additional caching method calls

--- server.js
+++ server.js
@@ -7,32 +7,40 @@ const port        = process.env.PORT || 8080;
 const refreshRate = 900000; //15 minutes
 const launchURL   = "https://launchlibrary.net/1.2/launch/next/50";
 
-// In-memory storage
-let launchData = {};
-
 // Server setup
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Function that runs the promise to update the cache
-function cachingLaunchData(url){
+// In-memory storage
+let launchData = {};
+
+// Functions
+function queryData(url){
   getLaunchData(url).then((result) => {
     launchData = result;
   })
 }
 
-// Cache data in memory during initial server startup and refresh once every
-// arbitrary amount of time
-cachingLaunchData(launchURL);
-setInterval(cachingLaunchData.bind(null, launchURL), refreshRate);
+function cachingLaunchData(url){
+  if(!launchData.data){
+    queryData(url);
+  } else {
+    console.log("fetch from cache");
+  }
+}
 
 // Routes
 app.get("/", (req, res, next) => {
+  cachingLaunchData(launchURL, launchData);
   res.render("index", launchData);
 })
 
 app.listen(port, () => {
+  // Cache data in memory during initial server startup and refresh once every
+  // arbitrary amount of time
+  cachingLaunchData(launchURL);
+  setInterval(queryData.bind(null, launchURL), refreshRate);
   console.log(`Example app listening on port ${port}!`);
 });
