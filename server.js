226fdf42e2a6f70900dafe3d391d1b2db77fc2f9
Yohan Wiputra <yowiputra@gmail.com>
2017-08-29 19:54:51 +0000

    cleaned up promises methods

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express    = require("express");
-const bodyParser = require("body-parser");
+const express       = require("express");
+const bodyParser    = require("body-parser");
 
 const getLaunchData = require("./utils/getLaunchData.js");
 
-const port        = process.env.PORT || 8080;
-const refreshRate = 900000; //15 minutes
-const launchURL   = "https://launchlibrary.net/1.2/launch/next/50";
+const port          = process.env.PORT || 8080;
+const refreshRate   = 900000; //15 minutes
+const launchURL     = "https://launchlibrary.net/1.2/launch/next/50";
 
 // Server setup
 const app = express();
@@ -16,37 +16,25 @@ app.use(bodyParser.urlencoded({extended: true}));
 // In-memory storage
 let launchData = {};
 
-// Caching functions
-function queryData(url){
-  getLaunchData(url).then((result) => {
-    launchData = result;
-  })
-}
-
-function cachingLaunchData(url){
-  return new Promise(function(resolve, reject) {
-    if(!launchData.data){
-      queryData(url);
-    } else {
-      console.log("fetch from cache");
-    }
-    resolve();
-  });
+// Caching function
+function queryAndCacheData(url){
+  return getLaunchData(url)
+    .then((result) => {
+      console.log("setting cache...");
+      launchData = result;
+    });
 }
 
 // Routes
 app.get("/", (req, res, next) => {
-  // redundancy in case data doesn't load during server startup
-  cachingLaunchData(launchURL).then(() => {
-    res.render("index", launchData);
-  })
+  res.render("index", launchData);
 })
 
-app.listen(port, () => {
-  // Cache data in memory during initial server startup and refresh once every
-  // arbitrary amount of time
-  cachingLaunchData(launchURL).then(() => {
-    setInterval(queryData.bind(null, launchURL), refreshRate);
-    console.log(`Example app listening on port ${port}!`);
-  })
-});
+queryAndCacheData(launchURL)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}!`);
+      // refresh cache at an arbitrary rate
+      setInterval(queryAndCacheData.bind(null, launchURL), refreshRate);
+    });
+  });
--- utils/getLaunchData.js
+++ utils/getLaunchData.js
@@ -1,18 +1,12 @@
 const fetch = require("node-fetch");
 
 module.exports = (url) => {
-  return new Promise ((resolve, reject) => {
-    console.log("fetch from api");
-    fetch(url)
+  return fetch(url)
     .then(res => res.json())
-    .then(results => {
+    .then(({launches}) => {
       const templateVars = {
-        data: results.launches,
+        data: launches,
       }
-      resolve(templateVars);
+      return templateVars;
     })
-    .catch(err => {
-      reject(err);
-    });
-  })
 }
